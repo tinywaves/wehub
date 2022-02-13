8258789d409cef9bcb61c66433e4bfc56297680b
tinyRipple <zheng.dong.hui@foxmail.com>
2022-02-13 16:37:34 +0800

    chore: change type:any to type:unknown

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,7 +1,7 @@
 // A custom hook that debounce.
 import { useEffect, useState } from 'react';
 
-const useDebounce = (value: any, delay?: number) => {
+const useDebounce = (value: unknown, delay?: number) => {
   const [debounceValue, setDebounceValue] = useState(value);
 
   useEffect(() => {
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
 // Determine if a value is a invalid value.(0 is a valid value)
-export const isInvalid = (value: any) => (value === 0 ? false : !value);
+export const isInvalid = (value: unknown) => (value === 0 ? false : !value);
 
 // Remove value attributes.
 export const cleanObject = (object: object) => {
