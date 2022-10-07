f3671eda6c157dd559f30a4c514748d8f8be286c
tinyRipple <isdhzh@gmail.com>
2022-10-08 03:20:41 +0800

    chore: install jira-dev-tool

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { loadDevTools } from 'jira-dev-tool';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(<App />);
+loadDevTools(() => {
+  const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+  );
+  root.render(<App />);
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
