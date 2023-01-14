09635ea5659b771c0217ec1fd849f0fa67da8213
Denver Cheng <isdhzh@gmail.com>
2023-01-14 22:40:44 +0800

    chore: fix styles

--- src/pages/project-list/project-list-table.tsx
+++ src/pages/project-list/project-list-table.tsx
@@ -1,40 +1,37 @@
 import dayjs from 'dayjs';
 
-import { Table, Container } from '@mantine/core';
+import { Table } from '@mantine/core';
 
 import { ProjectListTableProps } from './interface';
 
 const ProjectListTable = ({ projectList, users }: ProjectListTableProps) => {
   return (
-    <Container size="sm">
-      <Table striped highlightOnHover>
-        <thead>
-          <tr>
-            <th>Project Name</th>
-            <th>Apartment</th>
-            <th>Manager</th>
-            <th>Created Time</th>
+    <Table striped highlightOnHover>
+      <thead>
+        <tr>
+          <th>Project Name</th>
+          <th>Apartment</th>
+          <th>Manager</th>
+          <th>Created Time</th>
+        </tr>
+      </thead>
+      <tbody>
+        {projectList.map(project => (
+          <tr key={project.id}>
+            <td>{project.name}</td>
+            <td>{project.organization}</td>
+            <td>
+              {users.find(user => user.id === project.personId)?.name || 'NULL'}
+            </td>
+            <td>
+              {project.created
+                ? dayjs(project.created).format('YYYY-MM-DD')
+                : 'NULL'}
+            </td>
           </tr>
-        </thead>
-        <tbody>
-          {projectList.map(project => (
-            <tr key={project.id}>
-              <td>{project.name}</td>
-              <td>{project.organization}</td>
-              <td>
-                {users.find(user => user.id === project.personId)?.name ||
-                  'NULL'}
-              </td>
-              <td>
-                {project.created
-                  ? dayjs(project.created).format('YYYY-MM-DD')
-                  : 'NULL'}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-    </Container>
+        ))}
+      </tbody>
+    </Table>
   );
 };
 
