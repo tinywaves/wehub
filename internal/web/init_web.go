dfa97ca7184ad13a8ce0b870f5b65efbd29d2119
tinywaves <dhzhme@gmail.com>
2024-04-14 00:20:19 +0800

    feat: add cors middleware.

--- internal/web/init_web.go
+++ internal/web/init_web.go
@@ -1,12 +1,23 @@
 package web
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"strings"
+	"time"
 	"wehub/internal/web/user"
 )
 
 func InitWeb() *gin.Engine {
 	server := gin.Default()
+	server.Use(cors.New(cors.Config{
+		AllowHeaders:     []string{"Content-Type", "Authorization"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return strings.Contains(origin, "http://localhost") || origin == "https://my-website.com"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 	mainRouter := server.Group("/v1/api")
 
 	userHandler := user.InitHandler()
