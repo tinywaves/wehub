daae3d9c4aeaee84be448b991a2c2d577133123c
dhzhme <dhzhme@gmail.com>
2024-07-24 20:24:35 +0800

    fix: user routes

--- internal/web/user.go
+++ internal/web/user.go
@@ -7,10 +7,10 @@ type UserHandler struct{}
 
 func (u *UserHandler) RegisterRoutes(rootRouter *gin.RouterGroup) {
 	userRouter := rootRouter.Group("/user")
-	userRouter.POST("/user/sign-up", u.SignUp)
-	userRouter.POST("/user/sign-in", u.SignIn)
-	userRouter.PUT("/user/:id", u.Edit)
-	userRouter.GET("/user/:id", u.Get)
+	userRouter.POST("/sign-up", u.SignUp)
+	userRouter.POST("/sign-in", u.SignIn)
+	userRouter.PUT("/:id", u.Edit)
+	userRouter.GET("/:id", u.Get)
 }
 
 func (u *UserHandler) SignUp(ctx *gin.Context) {}
